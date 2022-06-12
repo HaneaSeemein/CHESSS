5d1146745f27999254984fd3bcb053091cee0dd7
HaneaSeemein <haniya23seemein@gmail.com>
2022-06-13 04:43:29 +0530

    this is so awesomegit add base.html javascript.js! i almost made it! just left with few more simple(kfc) functions

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@ function valid(id, curr) {
     color = limitcolor.charAt(0);
     if (limitcolor == "none") return true;
     else if (limitcolor == currentcolor) return false;
-    else if (limitcolor != currentcolor) return true;
+    // else if (limitcolor != currentcolor) return true;
 }
 
 function knightmove(current) {
@@ -154,81 +154,129 @@ function wpawnmove(current) {
     var nextid = [];
     if (valid(curr + 10, curr))
         nextid.push(curr + 10);
-    if (parseInt(curr / 10) == 2)
-        if (valid(curr + 20, curr))
-            nextid.push(curr + 20);
+    if (parseInt(curr / 10) == 2 && valid(curr + 20, curr))
+        nextid.push(curr + 20);
     return nextid
 }
 
 function bpawnmove(current) {
     var curr = parseInt(current);
     var nextid = [];
-    if (valid(curr + 10, curr)) nextid.push(curr - 10);
-    if (parseInt(curr / 10) == 2)
-        if (valid(curr - 20, curr)) nextid.push(curr - 20);
+    if (valid(curr - 10, curr)) nextid.push(curr - 10);
+    if (parseInt(curr / 10) == 7 && valid(curr - 20, curr)) nextid.push(curr - 20);
     return nextid
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// var moved = false;
-// function play(c) {
-var firstrow = true;
-// if (c == "white") {}
+var step = "black";
+var type = "none";
+var ID = 0;
+var allowedboxes = [];
 var blocks = document.querySelectorAll(".blocks li button");
+// alert(blocks.length);
 for (let index = 0; index < blocks.length; index++) {
     blocks[index].addEventListener("click", function() {
-        let type = this.getAttribute("value");
-        var ID = this.getAttribute("id");
-        let allowedmoves = []
-        if (type === "wpawn") {
-            allowedmoves = wpawnmove(ID);
-        } else if (type === "bpawn") {
-            allowedmoves = bpawnmove(ID);
-        } else if (type === "wrook" || type === "brook") {
-            allowedmoves = rookmove(ID);
-        } else
-        if (type === "wbishop" || type === "bbishop") {
-            allowedmoves = bishopmove(ID);
-        } else if (type === "wknight" || type === "bknight") {
-            allowedmoves = knightmove(ID);
-        } else if (type === "wking" || type === "bking") {
-            allowedmoves = kingmove(ID);
-        } else if (type === "wkween" || type === "bkween") {
-            allowedmoves = queenmove(ID);
-        }
-        // alert("checkpoint after allowed moves if else blocks");
-        allowedboxes = [];
-        for (let index = 0; index < allowedmoves.length; index++) {
-            let box = eval("document.getElementById(" + allowedmoves[index] + ")");
-            allowedboxes.push(box);
-        }
-        /////////////////////////////FAULTY////////////////////////////////////////////
-        // let backbtn = document.querySelector("#back");
-        // backbtn.classList.add("highlight");
-        for (let index = 0; index < allowedboxes.length; index++) {
-            moved = false;
-            allowedboxes[index].classList.add("highlight");
-            allowedboxes[index].addEventListener("click", function() {
-                if (!moved) {
-                    this.setAttribute("value", type);
-                    let old = eval("document.getElementById(" + ID + ")");
-                    old.setAttribute("value", "none");
-                    moved = true;
-                    index = allowedboxes.length;
+        switch (step) {
+            case "white":
+                {
+                    type = this.getAttribute("value");
+                    ID = this.getAttribute("id");
+                    let allowedmoves = [];
+                    allowedboxes = [];
+                    if (type === "wpawn") {
+                        step = "wmove";
+                        allowedmoves = wpawnmove(ID);
+                    } else if (type === "wrook") {
+                        step = "wmove";
+                        allowedmoves = rookmove(ID);
+                    } else if (type === "wbishop") {
+                        step = "wmove";
+                        allowedmoves = bishopmove(ID);
+                    } else if (type === "wknight") {
+                        step = "wmove";
+                        allowedmoves = knightmove(ID);
+                    } else if (type === "wking") {
+                        step = "wmove";
+                        allowedmoves = kingmove(ID);
+                    } else if (type === "wkween") {
+                        step = "wmove";
+                        allowedmoves = queenmove(ID);
+                    }
+                    for (let index = 0; index < allowedmoves.length; index++) {
+                        let box = eval("document.getElementById(" + allowedmoves[index] + ")");
+                        allowedboxes.push(box);
+                        box.classList.add("highlight");
+                    }
                 }
-                allowedboxes = [];
-                ////////////////////FAULTY//////////////////////////////////////////
-                for (let index = 0; index < allowedboxes.length; index++) {
-                    allowedboxes[index].classList.remove("highlight");
+                break;
+            case "wmove":
+                {
+                    let oID = this.getAttribute("id");
+                    for (let index = 0; index < allowedboxes.length; index++) {
+                        allowedboxes[index].classList.remove("highlight");
+                        let theID = allowedboxes[index].getAttribute("id");
+                        if (oID == theID) {
+                            this.setAttribute("value", type);
+                            let old = eval("document.getElementById(" + ID + ")");
+                            old.setAttribute("value", "none");
+                            step = "black";
+                        }
+                    }
                 }
-            });
+                break;
+            case "black":
+                {
+                    type = this.getAttribute("value");
+                    ID = this.getAttribute("id");
+                    let allowedmoves = [];
+                    allowedboxes = [];
+                    if (type === "bpawn") {
+                        step = "bmove";
+                        allowedmoves = bpawnmove(ID);
+                    } else if (type === "brook") {
+                        step = "bmove";
+                        allowedmoves = rookmove(ID);
+                    } else if (type === "bbishop") {
+                        step = "bmove";
+                        allowedmoves = bishopmove(ID);
+                    } else if (type === "bknight") {
+                        step = "bmove";
+                        allowedmoves = knightmove(ID);
+                    } else if (type === "bking") {
+                        step = "bmove";
+                        allowedmoves = kingmove(ID);
+                    } else if (type === "bkween") {
+                        step = "bmove";
+                        allowedmoves = queenmove(ID);
+                    }
+                    for (let index = 0; index < allowedmoves.length; index++) {
+                        let box = eval("document.getElementById(" + allowedmoves[index] + ")");
+                        allowedboxes.push(box);
+                        box.classList.add("highlight");
+                    }
+                }
+                break;
+            case "bmove":
+                {
+                    let oID = this.getAttribute("id");
+                    for (let index = 0; index < allowedboxes.length; index++) {
+                        allowedboxes[index].classList.remove("highlight");
+                        let theID = allowedboxes[index].getAttribute("id");
+                        if (oID == theID) {
+                            this.setAttribute("value", type);
+                            let old = eval("document.getElementById(" + ID + ")");
+                            old.setAttribute("value", "none");
+                            step = "white";
+                        }
+                    }
+                }
+                break;
+                // default:
+                //     break;
         }
-    });
+
+    })
 }
-// }
-
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // function rookmov(current) {
@@ -259,3 +307,109 @@ for (let index = 0; index < blocks.length; index++) {
 //     }
 //     this.classList.remove("highlight");
 // })
+
+
+// var moved = false;
+// function play(c) {
+//     var firstrow = true;
+//     if (c == "white") {}
+//     var blocks = document.querySelectorAll(".blocks li button");
+//     for (let index = 0; index < blocks.length; index++) {
+//         blocks[index].addEventListener("click", function() {
+//             let type = this.getAttribute("value");
+//             var ID = this.getAttribute("id");
+//             let allowedmoves = []
+//             if (type === "wpawn") {
+//                 allowedmoves = wpawnmove(ID);
+//             } else if (type === "bpawn") {
+//                 allowedmoves = bpawnmove(ID);
+//             } else if (type === "wrook" || type === "brook") {
+//                 allowedmoves = rookmove(ID);
+//             } else
+//             if (type === "wbishop" || type === "bbishop") {
+//                 allowedmoves = bishopmove(ID);
+//             } else if (type === "wknight" || type === "bknight") {
+//                 allowedmoves = knightmove(ID);
+//             } else if (type === "wking" || type === "bking") {
+//                 allowedmoves = kingmove(ID);
+//             } else if (type === "wkween" || type === "bkween") {
+//                 allowedmoves = queenmove(ID);
+//             }
+//             // alert("checkpoint after allowed moves if else blocks");
+//             allowedboxes = [];
+//             for (let index = 0; index < allowedmoves.length; index++) {
+//                 let box = eval("document.getElementById(" + allowedmoves[index] + ")");
+//                 allowedboxes.push(box);
+//             }
+//             /////////////////////////////FAULTY////////////////////////////////////////////
+//             // let backbtn = document.querySelector("#back");
+//             // backbtn.classList.add("highlight");
+//             for (let index = 0; index < allowedboxes.length; index++) {
+//                 moved = false;
+//                 allowedboxes[index].classList.add("highlight");
+//                 allowedboxes[index].addEventListener("click", function() {
+//                     if (!moved) {
+//                         this.setAttribute("value", type);
+//                         let old = eval("document.getElementById(" + ID + ")");
+//                         old.setAttribute("value", "none");
+//                         moved = true;
+//                         index = allowedboxes.length;
+//                     }
+//                     allowedboxes = [];
+//                     ////////////////////FAULTY//////////////////////////////////////////
+//                     for (let index = 0; index < allowedboxes.length; index++) {
+//                         allowedboxes[index].classList.remove("highlight");
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// }
+
+
+// if (step == "white") {
+//     type = this.getAttribute("value");
+//     ID = this.getAttribute("id");
+//     let allowedmoves = [];
+//     allowedboxes = [];
+//     if (type === "wpawn") {
+//         step = "move";
+//         allowedmoves = wpawnmove(ID);
+//     } else if (type === "bpawn") {
+//         step = "move";
+//         allowedmoves = bpawnmove(ID);
+//     } else if (type === "wrook" || type === "brook") {
+//         step = "move";
+//         allowedmoves = rookmove(ID);
+//     } else if (type === "wbishop" || type === "bbishop") {
+//         step = "move";
+//         allowedmoves = bishopmove(ID);
+//     } else if (type === "wknight" || type === "bknight") {
+//         step = "move";
+//         allowedmoves = knightmove(ID);
+//     } else if (type === "wking" || type === "bking") {
+//         step = "move";
+//         allowedmoves = kingmove(ID);
+//     } else if (type === "wkween" || type === "bkween") {
+//         step = "move";
+//         allowedmoves = queenmove(ID);
+//     }
+//     for (let index = 0; index < allowedmoves.length; index++) {
+//         let box = eval("document.getElementById(" + allowedmoves[index] + ")");
+//         allowedboxes.push(box);
+//         box.classList.add("highlight");
+//     }
+// } else if (step == "move") {
+//     let oID = this.getAttribute("id");
+//     for (let index = 0; index < allowedboxes.length; index++) {
+//         allowedboxes[index].classList.remove("highlight");
+//         let theID = allowedboxes[index].getAttribute("id");
+//         if (oID == theID) {
+//             this.setAttribute("value", type);
+//             let old = eval("document.getElementById(" + ID + ")");
+//             old.setAttribute("value", "none");
+//             step = "";
+//         }
+
+//     }
+// }
