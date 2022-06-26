b8775d45746a65f56c640ca14c7f9c3b59a5557f
HaneaSeemein <haniya23seemein@gmail.com>
2022-06-27 01:18:36 +0530

    added express.js and some tiny changes

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const app = express();
 // app.sendFile("base.html")
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/base.html");
-    // res.sendFile(__dirname + "/javascript.js");
-    // res.sendFile(__dirname + "/style.css");
 });
+app.use('/', express.static(__dirname + "/public"));
 
+// app.get('/:username', function(req, res) {
+//     console.log("hey " + req.params.username);
+// });
 app.get("/contact", function(req, res) {
     res.send("<script>alert('haniya23seemein@gmail.com')</script>");
 });
 
-app.listen("3000", function(req, res) {
+app.listen("8080", function(req, res) {
     console.log("listening")
 });
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+
+// app.get('/:username', function(req, res) {
+//     console.log("hey " + req.params.username);
+// });
